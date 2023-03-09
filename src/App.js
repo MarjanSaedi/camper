5cfac2c7307fe0f5d37ec195f12e1f7bc63d99ef
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-03-08 19:30:51 -0700

    sign up component from MUI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/SignUp'
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Hello world
-        </p>
-
+      <SignUp />
     </div>
   );
 }
