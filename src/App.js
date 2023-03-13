e51d442862c929210367b24ae4e5347cc97875ba
Marjan <marjan.saedi@gmail.com>
2023-03-13 12:23:26 -0600

    InitialLoginPageDesign

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-        <label>Email</label>
-        <input type='text' placeholder='Enter your email...'/>
-        <label>Password</label>
-        <input type='Password' placeholder='Enter your password...'/>
-        <input type= 'checkbox'/>
-        <label>Remember me</label>
-        <label>Forget password</label>
-        <button>Login</button>
-        <button>Sign up</button>
+    <div id="root">
+      <div class="root-box css-rootbox1">
+        <div className="main-container">
+          <div className="App">
+            <label className="Title">Sing in </label>
+
+            <label>Email</label>
+            <input type="text" placeholder="Enter your email..." />
+
+            <label>Password</label>
+            <input type="Password" placeholder="Enter your password..." />
+            <div className="rootRows">
+              <input type="checkbox" />
+              <label>Remember me</label>
+
+              <label>Forget password</label>
+            </div>
+            <div className="rootRows">
+              <button>Login</button>
+              <button>Sign up</button>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
