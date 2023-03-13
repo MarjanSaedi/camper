133b3205b9261b176cdc6ae473349d19b8431e22
Marjan <marjan.saedi@gmail.com>
2023-03-13 15:20:05 -0600

    TemporaryLoginPageDesign

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ function App() {
         <div className="main-container">
           <div className="App">
             <label className="Title">Sing in </label>
-
-            <label>Email</label>
-            <input type="text" placeholder="Enter your email..." />
-
-            <label>Password</label>
-            <input type="Password" placeholder="Enter your password..." />
-            <div className="rootRows">
-              <input type="checkbox" />
-              <label>Remember me</label>
-
-              <label>Forget password</label>
+            <div className="topPadding">
+              <label>Email</label>
+              <input type="text" class= "RootInputs" placeholder="Enter your email..." />
             </div>
-            <div className="rootRows">
-              <button>Login</button>
-              <button>Sign up</button>
+            <div className="topPadding">
+              <label>Password</label>
+              <input type="Password" class= "RootInputs" placeholder="Enter your password..." />
+            </div>
+            <div className="topPadding">
+              <div>
+                <input type="checkbox" />
+                <label>Remember me</label>
+                <label id="forgetPass">Forget password</label>
+              </div>
+              <button class="RooButton">Login</button>
+            </div>
+            <div className="topPadding">
+              <label>Register now</label>
+              <button class="RooButton">Sign up</button>
+              
             </div>
           </div>
           <div></div>
